9262591dc89e78ba722a79bd1be75541171c63da
Aerttyz <alesandrosilva2004@gmail.com>
2025-01-07 20:01:44 -0300

    start API

---
+++ secure/src/main/java/com/face/secure/FaceSecureApplication.java
@@ -0,0 +1,13 @@
+package com.face.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceSecureApplication.class, args);
+	}
+
+}
