e6ea6ca1a9006f14d638148612809e1815b09746
Raimdrs <raiimdrs@gmail.com>
2025-01-21 23:58:34 -0300

    codigo Teste AHHAHAHAHHA

--- secure/src/main/java/com/face/secure/config/AppConfig.java
+++ secure/src/main/java/com/face/secure/config/AppConfig.java
@@ -7,10 +7,9 @@ import org.opencv.core.Core;
 @Configuration
 public class AppConfig {
 
-    // Este bean carrega a biblioteca nativa do OpenCV na inicialização da aplicação
     @Bean
     public Core loadOpenCV() {
-        // Carrega a biblioteca nativa do OpenCV
+
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         return new Core();
     }
