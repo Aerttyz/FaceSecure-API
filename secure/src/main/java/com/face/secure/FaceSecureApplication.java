64a6e700707751e50bf37b485ff496f50284e815
Aerttyz <alesandrosilva2004@gmail.com>
2025-02-13 02:14:29 -0300

    fix: rename app class

--- secure/src/main/java/com/face/secure/FaceSecureApplication.java
+++ secure/src/main/java/com/face/secure/FaceSecureApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecureApplication {
+public class FaceSecureApplication {
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		System.out.println("OpenCV loaded successfully" + Core.VERSION);
-		SpringApplication.run(SecureApplication.class, args);
+		SpringApplication.run(FaceSecureApplication.class, args);
 	}
 
 }
